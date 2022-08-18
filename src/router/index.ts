14b8bcc552aef88763ddd4f95098e339068e8e43
koreloop <105968622+koreloop@users.noreply.github.com>
2022-08-18 20:38:43 +0800

    Add files via upload

---
+++ src/router/index.ts
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/home.vue'
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/config',
+    name: 'config',
+    component: () => import('../views/config.vue')
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: () => import('../views/about.vue')
+  }
+]
+
+const router = new VueRouter({
+  mode: 'hash',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
